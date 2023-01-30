b4d340db476c1ef06b7e15791ef4aec7cfda78a3
TDI2282 <113158980+TDI2282@users.noreply.github.com>
2023-01-30 15:21:43 +0200

    Add files via upload

--- java/com/tdi/tmaps/utils/Common.kt
+++ java/com/tdi/tmaps/utils/Common.kt
@@ -1,6 +1,6 @@
 package com.tdi.tmaps.utils
 
-import com.tdi.tmaps.model.User
+import com.tdi.tmaps.model.*
 import com.tdi.tmaps.remote.IFCMService
 import com.tdi.tmaps.remote.RetrofitClient
 import java.sql.Timestamp
@@ -16,6 +16,16 @@ object Common {
         return SimpleDateFormat("dd-MM-yyyy HH:mm").format(date).toString()
     }
 
+    val EV_STATION: String = "EvStation"
+    var evInfo: EvStation ?= null
+    val FS_STATION: String = "FsStation"
+    var fsInfo: FuelStation ?= null
+    val CAR_WASH: String = "CarWash"
+    var washInfo: CarWash ?= null
+    val CAR_SERVICE: String = "CarService"
+    var serviceInfo: CarService ?= null
+    val CAR_TIRE: String = "CarTire"
+    var tireInfo: CarTires ?= null
     var USER_INFO: String = "UserInformation"
     var loggedUser: User? = null
     var trackingUser: User? = null

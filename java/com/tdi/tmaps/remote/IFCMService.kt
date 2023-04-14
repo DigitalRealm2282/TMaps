b595d210092f3b9cf1202fc8727d23568d94225c
Digital Realm <113158980+DigitalRealm2282@users.noreply.github.com>
2023-04-14 03:51:23 +0200

    Update IFCMService.kt

--- java/com/tdi/tmaps/remote/IFCMService.kt
+++ java/com/tdi/tmaps/remote/IFCMService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.POST
 interface IFCMService {
     @Headers(
         "Content-Type:application/json",
-        "Authorization: key=AAAAvH2ruLE:APA91bHXcABrWCk0Eo5Va4vkpMUjZNbaV2zoO0OiglTPZrHSEg2FMkSm_t3VPJKrPArsec5FhVDbw3-f_mPXHPCnNsJ36LzRQjo7AT20t1BAa-bsHsq_8v7SxtD9MPGRXygxR1-0X9Uj"
+        "Authorization: key=AAAA"
     )
 
     @POST("fcm/send")
